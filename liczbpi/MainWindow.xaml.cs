5d1998c410ff07dc7fb7c5a1e288714dc02c44e9
przybylski.pawel <przybylski.pawel@STACJA105-04>
2024-04-17 13:51:25 +0200

    Dodaj pliki projektów.

---
+++ liczbpi/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace liczbpi
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void obliczButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(promienTextBox.Text, out double promien) &&
+              
+                int.TryParse(punktyTextBox.Text, out int liczbaPunktow))
+            {
+                double punktyWewnatrzKola = 0;
+                Random random = new Random();
+
+                for (int i = 0; i < liczbaPunktow; i++)
+                {
+                    double x = random.NextDouble() * (2 * promien) - promien;
+                    double y = random.NextDouble() * (2 * promien) - promien;
+
+                    if (Math.Pow(x, 2) + Math.Pow(y, 2) <= Math.Pow(promien, 2))
+                    {
+                        punktyWewnatrzKola++;
+                    }
+                }
+
+                double przyblizonaWartoscPi = (4 * punktyWewnatrzKola) / liczbaPunktow;
+                piResultLabel.Content = $"Przybliżona wartość Pi: {przyblizonaWartoscPi}";
+            }
+            else
+            {
+                MessageBox.Show("Nie podałeś wszystkich danych", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
